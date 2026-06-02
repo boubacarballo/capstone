ac548b00a8ff06bda3d775889ff2a194e8a23ebe
Boubacar Ballo <bab9755@nyu.edu>
2026-06-02 15:21:09 +0400

    cleanup

--- test.py
+++
@@ -1,8 +0,0 @@
-import ssl, certifi, nltk
-
-# Patch the SSL context to use certifi's CA bundle
-ssl._create_default_https_context = lambda: ssl.create_default_context(cafile=certifi.where())
-
-# Now download tokenizers safely
-nltk.download("punkt")
-nltk.download("punkt_tab")
--- test_metrics.py
+++
@@ -1,7 +0,0 @@
-from metrics import compute_final_score, compute_bert_score, compute_score
-from runtime_config import get_ground_truth_bundle
-
-agent_summary = """The event features busy, energetic career services. Counselors offer individual guidance. Workshops focus on career and interview skills. A career fair collects student resumes. Recruiters facilitate connections between students and employers."""
-ground_truth_text = get_ground_truth_bundle().get("text", "")
-score = compute_score(agent_summary, ground_truth_text)
-print(score)
